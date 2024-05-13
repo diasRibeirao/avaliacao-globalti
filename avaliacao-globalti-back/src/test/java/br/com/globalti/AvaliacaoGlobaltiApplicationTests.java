dd18806852d9ee5f1daa2252cb5ded95ffe039e3
Emerson de Oliveira <emersondiaspd@gmail.com>
2024-05-13 18:14:10 -0300

    Iniciando projeto backend spring boot java

---
+++ avaliacao-globalti-back/src/test/java/br/com/globalti/AvaliacaoGlobaltiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globalti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoGlobaltiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
